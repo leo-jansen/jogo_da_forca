a48a3f2a8eae2f0f7e9bfa752025b8eba50f8a18
leo-jansen <janseleo@gmail.com>
2021-01-28 16:53:33 -0300

    Abertura do jogo

---
+++ forca.py
@@ -0,0 +1,10 @@
+def jogar():
+  imprime_mensagem_abertura()
+
+def imprime_mensagem_abertura():
+  print("*********************************")
+  print("***Bem vindo ao jogo da Forca!***")
+  print("*********************************")
+
+if __name__ == "__main__":
+  jogar()
