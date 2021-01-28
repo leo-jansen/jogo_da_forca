b391e3d38b71a6a7bc2f977c9c074569e7c08f6d
leo-jansen <janseleo@gmail.com>
2021-01-28 17:47:38 -0300

    Le arquivo e sortear palavra

--- forca.py
+++ forca.py
@@ -1,10 +1,36 @@
 def jogar():
   imprime_mensagem_abertura()
+  palavras_e_dicas = le_arquivo()
+  numero_sorteado = sortear_numero(palavras_e_dicas)
+  palavra_secreta = carrega_palavra_secreta(palavras_e_dicas, numero_sorteado)
 
 def imprime_mensagem_abertura():
   print("*********************************")
   print("***Bem vindo ao jogo da Forca!***")
   print("*********************************")
 
+def le_arquivo():
+  arquivo = open("palavras.txt", "r")
+  palavras_e_dicas = []
+  for linha in arquivo:
+    palavras_e_dicas.append(linha)
+  arquivo.close()
+  return palavras_e_dicas
+
+def sortear_numero(palavras_e_dicas):
+  numero_sorteado = random.randrange(0,len(palavras_e_dicas))
+  return numero_sorteado
+
+def carrega_palavra_secreta(palavras_e_dicas, numero_sorteado):
+  palavra_e_dica = palavras_e_dicas[numero_sorteado]
+  palavra_e_dica = palavra_e_dica.split(",")
+  indice = 0
+  for elemento in palavra_e_dica:
+    if indice == 0:
+      elemento = elemento.strip()
+      palavra = elemento
+    indice += 1
+  return palavra.upper()
+  
 if __name__ == "__main__":
   jogar()
